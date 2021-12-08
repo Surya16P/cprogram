e2458cc661e577bfa015567deb9a0dbbfea1a7de
Surya16P <surya1552000gmail.com>
2021-12-08 11:46:46 +0530

    count char in INPUT

---
+++ pgm6.c
@@ -0,0 +1,26 @@
+/* count char in input */
+#include<stdio.h>
+/*
+int main()
+{
+	long a = 0;
+
+	while(getchar() != EOF)
+		++a;
+	printf("%ld\n",a);
+	return 0;
+}
+
+*/
+
+int main()
+	{
+		double a ;
+		for(a = 0;getchar() != EOF;++a)
+		         ;
+			printf("%lf",a);
+		return 0;
+	}
+
+			
+
